d1ef310435d18d38e47f85dc42d50b309b4abb84
Patricia Correia <pncorreia25@gmail.com>
2022-05-23 17:46:53 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return data.species.find((ell) => ell.name === animal).residents
+    .every((obj) => obj.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
