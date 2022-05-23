1933335a356a4cd79fdf4ef67531cd5450948967
Patricia Correia <pncorreia25@gmail.com>
2022-05-23 17:54:01 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return data.employees
+    .find((ells) => employeeName === ells.firstName || employeeName === ells.lastName);
 }
 
 module.exports = getEmployeeByName;
