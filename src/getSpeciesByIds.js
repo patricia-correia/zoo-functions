e103a7a8645612dd1ddf770aa830e3408ae04a0a
Patricia Correia <pncorreia25@gmail.com>
2022-05-23 17:39:38 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const array = data.species.filter((element) => ids.includes(element.id));
+  return array;
 }
 
 module.exports = getSpeciesByIds;
