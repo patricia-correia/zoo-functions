8d5a746a059fd1b5c865ba968d1bfe300250782c
Patricia Correia <pncorreia25@gmail.com>
2022-05-26 16:46:36 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
-
+  const child = entrants.filter((element) => element.age < 18);
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  const senior = entrants.filter((element) => element.age >= 50);
+  return {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui.
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  return (data.prices.child * child)
+   + (data.prices.adult * adult)
+   + (data.prices.senior * senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
