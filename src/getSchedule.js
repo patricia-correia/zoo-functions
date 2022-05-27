8169b0afa852143e1985f41474f2582f902aa284
Patricia Correia <pncorreia25@gmail.com>
2022-05-27 19:58:14 -0300

    requisito 12 e 13

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,34 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const { hours } = require('../data/zoo_data');
+
+function daysOpen(scheduleTarget) {
+  const week = species.filter((element) => element.availability
+    .find((ells) => ells === scheduleTarget));
+  const on = `Open from ${hours[scheduleTarget].open}am until ${hours[scheduleTarget].close}pm`;
+  if (scheduleTarget === 'Monday') {
+    return {
+      [scheduleTarget]: {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      },
+    };
+  }
+  return {
+    [scheduleTarget]: {
+      officeHour: on,
+      exhibition: week.map((ellem) => ellem.name),
+    },
+  };
+}
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (species.some((el) => el.name === scheduleTarget)) {
+    return species.find((ell) => ell.name === scheduleTarget).availability;
+  }
+  const days = daysOpen(scheduleTarget);
+  if (days !== undefined) {
+    return days;
+  }
 }
 
 module.exports = getSchedule;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,45 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Verifica se não passando argumentos retorna o objeto', () => {
+    const actual = getOpeningHours();
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+
+    expect(actual).toEqual(expected);
+  });
+  it('Retorna uma string para o argumento `Monday` e `09:00-AM`', () => {
+    const actual = getOpeningHours('Monday', '9:00-AM');
+    const expected = 'The zoo is closed';
+    expect(actual).toBe(expected);
+  });
+  it('Retorna uma string para o argumento `Tuesday` e `09:00-AM`', () => {
+    const actual = getOpeningHours('Tuesday', '09:00-AM');
+    const expected = 'The zoo is open';
+    expect(actual).toBe(expected);
+  });
+  it('Retorna uma string para o argumento `Wednesday` e `09:00-PM`', () => {
+    const actual = getOpeningHours('Wednesday', '09:00-PM');
+    const expected = 'The zoo is closed';
+    expect(actual).toBe(expected);
+  });
+  it('Retorna um erro para os argumentos `Thu`e `09:00-AM`', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrowError(new Error('The day must be valid. Example: Monday'));
+  });
+  it('Retorna um erro para os argumentos `Friday` e `09:00-ZM', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrowError(new Error('The abbreviation must be \'AM\' or \'PM\''));
+  });
+  it('Retorna um erro para os argumentos `Saturday` e `C9:00-AM`', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrowError(new Error('The hour should represent a number'));
+  });
+  it('Retorna um erro para os argumentos `Sunday` e `09:c0-AM`', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrowError(new Error('The minutes should represent a number'));
+  });
+});
