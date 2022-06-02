6b3ccf600680f48ec1a38a19a6ec39e8c0016afc
Patricia Correia <pncorreia25@gmail.com>
2022-06-02 12:37:31 -0300

    requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,9 +1,48 @@
-const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
+
+const hourOfDay = Object.values(data.hours);
+
+const days = Object.keys(data.hours);
+
+const animalTheZoo = data.species
+  .map((animal) => animal.name);
+
+const hoursItinerary = (daysWeekOpen) => {
+  const weekCalendar = {};
+  if (daysWeekOpen === 'Monday') {
+    weekCalendar.officeHour = 'CLOSED';
+    weekCalendar.exhibition = 'The zoo will be closed!';
+  } else {
+    const daysOpening = hourOfDay
+      .find((hour, index) => days[index] === daysWeekOpen);
+    const specie = data.species
+      .filter((element) => element.availability
+        .find((elle) => elle === daysWeekOpen));
+    weekCalendar.officeHour = `Open from ${daysOpening.open}am until ${daysOpening.close}pm`;
+    weekCalendar.exhibition = specie.map((element) => element.name);
+  }
+  return weekCalendar;
+};
+
+const scheduleWeek = () => days.reduce((acc, curr) =>
+  ({
+    ...acc,
+    [curr]: hoursItinerary(curr),
+
+  }), {});
+
+const animals = (animal) => data.species.find((specie) =>
+  specie.name === animal);
+
+const day = (param) => ({ [param]: hoursItinerary(param) });
 
 function getSchedule(scheduleTarget) {
-  const daysOpen = species
-    .filter((element) => scheduleTarget === element.animal);
-  return daysOpen;
+  if (animalTheZoo.includes(scheduleTarget)) {
+    return animals(scheduleTarget).availability;
+  } if (days.includes(scheduleTarget)) {
+    return day(scheduleTarget);
+  }
+  return scheduleWeek();
 }
 // {
 //    'Tuesday': {
@@ -11,6 +50,5 @@ function getSchedule(scheduleTarget) {
 //        'exhibition': ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
 //      },
 // };
-console.log(getSchedule('Tuesday'));
 
 module.exports = getSchedule;
